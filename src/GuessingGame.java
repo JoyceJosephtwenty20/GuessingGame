cf2a8e42839972056fe93a20731b085d07fc4ed8
joycejosephtwenty20 <joyce.j106s13@gmail.com>
2020-06-18 09:52:44 -0400

    changed line 9

--- src/GuessingGame.java
+++ src/GuessingGame.java
@@ -6,7 +6,7 @@ public class GuessingGame {
     public static void main(String[] args) {
         Random systemGuess = new Random();
         Scanner userInput = new Scanner(System.in);
-        int randomNumber = systemGuess.nextInt(9) + 1;
+        int randomNumber = systemGuess.nextInt(10) + 1;
         int userGuess;
         System.out.println("I am thinking about a number between 1 and 10..\nCan you guess it?");
         userGuess = userInput.nextInt();
